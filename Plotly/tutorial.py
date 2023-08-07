2464bb398b3e8c3578a821fe1e30db0a0cf43483
류민승 <ryuminseung96@gmail.com>
2023-08-07 17:05:52 +0900

    plotly study edited

--- Plotly/tutorial.py
+++ Plotly/tutorial.py
@@ -1,10 +1,18 @@
-from dash import Dash, html
+from dash import Dash, html, dash_table
+import pandas as pd
 
+#데이터 불러오기
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+#앱 시작하기
 app = Dash(__name__)
 
+# 앱 레이아웃
 app.layout = html.Div([
-    html.Div(children='HelloWorld')
+    html.Div(children='My First App with Data'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
 ])
 
+# 앱 실행하기
 if __name__ == '__main__':
     app.run(debug=True)
